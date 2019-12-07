2c2a743a71b50e1ba307407546033518a639ec31
Gerry Oei <goei3@gatech.edu>
2019-12-06 21:08:43 -0500

    Added circle packing filtering by tags

--- Result/src/js/circlepack.js
+++ Result/src/js/circlepack.js
@@ -1,89 +1,187 @@
-var width = 932;
-var height = 932;
-var all_tags = [];
+var width = 1000;
+var height = 1000;
+var current_dataset = [];
+var tagsSet = new Set(Object.keys(color_tags_mapping));
+var tagsSetAll = new Set(tagsSet);
+
+var packLayout = d3.pack()
+  .size([width, height]);
 
 var color = d3.scaleLinear()
     .domain([0, 5])
     .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
     .interpolate(d3.interpolateHcl)
 
-Promise.all([d3.json("./data/parentchildcompanies.json"),  d3.csv("./data/tags_list.csv") ]).then(function(dataset){
-  var format = d3.format(",d");
-
-  var data = dataset[0];
-  all_tags = dataset[1];
-
-	var root = d3.pack()
-              .size([width, height])
-              .padding(3)
-              (d3.hierarchy(data)
-                .sum(d => d.size)
-                .sort((a, b) => b.size - a.size))
-
-	let focus = root;
-  let view;
-
-	const svg = d3.select("svg.circlepack_svg")
-                .append("svg")
-                .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
-                .style("display", "block")
-                .style("margin", "0 -14px")
-                .style("background", color(0))
-                .style("cursor", "pointer")
-                .on("click", () => zoom(root));
-
-  const node = svg.append("g")
-                  .selectAll("circle")
-                  .data(root.descendants().slice(1))
-                  .join("circle")
-                  .attr("fill", d => d.children ? color(d.depth) : "white")
-                  .attr("pointer-events", d => !d.children ? "none" : null)
-                  .on("mouseover", function() { d3.select(this).attr("stroke", "red").attr("stroke-width",3); })
-                  .on("mouseout", function() { d3.select(this).attr("stroke", null); })
-                  .on("click", d => focus !== d && (zoom(d), d3.event.stopPropagation()));
-
-	const label = svg.append("g")
-                  .style("font", "10px sans-serif")
-                  .attr("pointer-events", "none")
-                  .attr("text-anchor", "middle")
-                  .selectAll("text")
-                  .data(root.descendants())
-                  .join("text")
-                  .style("fill-opacity", d => d.parent === root ? 1 : 0)
-                  .style("display", d => d.parent === root ? "inline" : "none")
-                  .text(d => d.data.name);
-
-	zoomTo([root.x, root.y, root.r * 2]);
-
-  function zoomTo(v) {
-    const k = width / v[2];
-
-    view = v;
-    label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-    node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-    node.attr("r", d => d.r * k);
-  }
-
-  function zoom(d) {
-    const focus0 = focus;
-
-    focus = d;
-
-    const transition = svg.transition()
-        .duration(d3.event.altKey ? 7500 : 750)
-        .tween("zoom", d => {
-          const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-          return t => zoomTo(i(t));
-        });
-
-    label
-      .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
-      .transition(transition)
-        .style("fill-opacity", d => d.parent === focus ? 1 : 0)
-        .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
-        .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
-  }
+Promise.all([d3.csv("./data/date_notnull_circle_pack.csv"),  d3.csv("./data/tags_list.csv") ]).then(function(dataset){
+  current_dataset.push(dataset[0]);
+  circlepack.generateCirclePack(current_dataset[0]);
 });
+
+class circlepack{
+    static addTag(tag) {
+        tagsSet.add(tag)
+        circlepack.clear();
+        circlepack.generateCirclePack(current_dataset[0]);
+    }
+
+    static resetTag() {
+        tagsSet = new Set(tagsSetAll)
+        circlepack.clear();
+        circlepack.generateCirclePack(current_dataset[0]);
+    }
+
+    static clearTag() {
+        tagsSet = new Set()
+        circlepack.clear();
+        circlepack.generateCirclePack(current_dataset[0]);
+    }
+
+    static removeTag(tag) {
+        tagsSet.delete(tag)
+        circlepack.clear();
+        circlepack.generateCirclePack(current_dataset[0]);
+    }
+
+    static clear(){
+      //Make radio buttons visible
+      $("#acquisition_circlepack").empty();
+    }
+
+    static generateCirclePack(dataset){
+
+      var format = d3.format(",d");
+
+      var data = dataset;
+
+      data = data.filter(function(d) {
+        //Turn string to array of tags
+        var tags = d['major_tags'];
+        tags = tags.replace('[','');
+        tags = tags.replace(']','');
+        tags = tags.replace(/'/g,'');
+        tags = tags.split(',');
+
+        //Check if tags set has that value
+        for (var i = 0; i < tags.length; i++){
+          if(tagsSet.has(tags[i])){
+            return d;
+          }
+        }
+
+        //return d;
+        // var year_string = year.toString();
+        // var sub_year_string = year_string.substring(2,4);
+        // if(d['displayName'] == companyName && d['startDate'].includes(sub_year_string)){
+        //   return d;
+        // }
+      });
+
+      console.log(data);
+
+      var nest = d3.nest()
+                   .key(function(d) { return d.displayName; })
+                   .entries(data);
+
+      nest = {
+        key: 'root',
+        values: nest
+      };
+
+      var root = d3.pack()
+                   .size([width, height])
+                   .padding(3)
+                   (d3.hierarchy(nest, function(d) {
+                        return d.values;
+                      }).sum(function(d) {
+                        return d['stocksSummary'] === undefined ? null : d['stocksSummary'];
+                      }).sort(function(a,b){
+                        return b['stocksSummary'] - a['stocksSummary'];
+                      }))
+
+      let focus = root;
+      let view;
+
+      const svg = d3.select("svg.circlepack_svg")
+                    .append("svg")
+                    .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+                    .style("display", "block")
+                    .style("margin", "0 -14px")
+                    .style("background", color(0))
+                    .style("cursor", "pointer")
+                    .on("click", () => zoom(root));
+
+      const node = svg.append("g")
+                      .selectAll("circle")
+                      .data(root.descendants().slice(1))
+                      .join("circle")
+                      .attr("fill", d => d.children ? color(d.depth) : "white")
+                      .on("mouseover", function(d) { 
+                        DetailsState.reset()
+                        if(d.parent === root){
+                          DetailsState.select(d.data.key)
+                        } else {
+                          DetailsState.select(d.data.name)
+                        }
+                        // //Make treemap
+                        // treemap.generateChart(d.data.key, ScatterplotState.state.year)
+
+                        // //Make barchart for ethnicity viz
+                        // ethnicity_barchart.generateChart(d.data.key, ScatterplotState.state.year)
+
+                        // //Make piechart for gender viz
+                        // gender_piechart.generateChart(d.data.key, ScatterplotState.state.year)
+                        d3.select(this).attr("stroke", "red").attr("stroke-width",3); 
+                      })
+                      .on("mouseout", function() { d3.select(this).attr("stroke", null); })
+                      .on("click", d => focus !== d && d.parent === root && (zoom(d), d3.event.stopPropagation()));
+
+      const label = svg.append("g")
+                      .style("font", "10px sans-serif")
+                      .attr("pointer-events", "none")
+                      .attr("text-anchor", "middle")
+                      .selectAll("text")
+                      .data(root.descendants())
+                      .join("text")
+                      .style("fill-opacity", d => d.parent === root ? 1 : 0)
+                      .style("display", d => d.parent === root ? "inline" : "none")
+                      .text(d =>  d.parent === root ? d.data.key : d.data.name);
+
+      if(data.length > 0){
+        zoomTo([root.x, root.y, root.r * 2]);
+      }
+
+      node.exit().remove();
+      
+      function zoomTo(v) {
+        const k = width / v[2];
+
+        view = v;
+        label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+        node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+        node.attr("r", d => d.r * k);
+      }
+
+      function zoom(d) {
+        const focus0 = focus;
+
+        focus = d;
+
+        const transition = svg.transition()
+            .duration(d3.event.altKey ? 7500 : 750)
+            .tween("zoom", d => {
+              const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+              return t => zoomTo(i(t));
+            });
+
+        label
+          .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+          .transition(transition)
+            .style("fill-opacity", d => d.parent === focus ? 1 : 0)
+            .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+            .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+      }
+    }
+}
 
 d3.selectAll(".acquisition_legend_shape").each(function (d) {
   const attribute = this.getAttribute("data-checkbox")
@@ -107,18 +205,17 @@ d3.selectAll(".acquisition_checkbox_svg").append("rect")
       d3.selectAll(".acquisition_checkbox_svg")
         .selectAll("rect")
         .attr("fill", selected ? "black" : "white")
-      // if (selected)
-      //   ScatterplotState.clearTag()
-      // else
-      //   ScatterplotState.resetTag()
+      if (selected)
+        circlepack.clearTag()
+      else
+        circlepack.resetTag()
       return
     }
 
-
-    // if (selected)
-    //   ScatterplotState.removeTag(attribute)
-    // else
-    //   ScatterplotState.addTag(attribute)
+    if (selected)
+      circlepack.removeTag(attribute)
+    else
+      circlepack.addTag(attribute)
 
   })
 
--- circlepack/csvtojson_circlepacking.py
+++ circlepack/csvtojson_circlepacking.py
@@ -4,18 +4,6 @@ import json
 result = []
 counter = 1
 parents = []
-logos = []
-
-with open('misc.csv') as csvfile:
-	readCSV = csv.reader(csvfile, delimiter=',')
-	header = next(readCSV)
-	for row in readCSV:
-		logo = row[5]
-		logo = logo.strip("{'url': '")
-		logo = logo.strip("'}")
-		company_name = row[3]
-		new = {"name": company_name, "logo" : logo}
-		logos.append(new)
 
 with open('date_notnull_circle_pack.csv') as csvfile:
 	readCSV = csv.reader(csvfile, delimiter=',')
@@ -24,28 +12,13 @@ with open('date_notnull_circle_pack.csv') as csvfile:
 		parent = row[0]
 		children = []
 		if parent not in parents:
-			child = {"name": row[3], "size": 3, "logo": ""}
-			if any(d.get('name', None) == row[3] for d in logos):
-				for d in logos:
-					if d['name'] == row[3]:
-						logo = {"logo" : d['logo']}
-						child.update(logo)
+			child = {"name": row[3], "size": 3, "date": row[1], "tags": row[5], "major_tags": row[6], "stocksSummary": row[7], "valuation": row[8]}
 			children.append(child)
-			company = {"name" : parent, "children" : children, "logo": ""}
-			if any(d.get('name', None) == row[0] for d in logos):
-				for d in logos:
-					if d['name'] == row[0]:
-						logo = {"logo" : d['logo']}
-						company.update(logo)
+			company = {"name" : parent, "children" : children,  "date": row[1], "tags": row[5], "major_tags": row[6], "stocksSummary": row[7], "valuation": row[8]}
 			result.append(company)
 			parents.append(parent)
 		elif parent in parents:
-			child = {"name": row[3], "size": 3, "logo" : ""}
-			if any(d.get('name', None) == row[3] for d in logos):
-				for d in logos:
-					if d['name'] == row[3]:
-						logo = {"logo" : d['logo']}
-						child.update(logo)
+			child = {"name": row[3], "size": 3, "date": row[1], "tags": row[5], "major_tags": row[6], "stocksSummary": row[7], "valuation": row[8]}
 			for comp in result:
 				if comp['name'] == parent:
 					comp['children'].append(child)
@@ -53,7 +26,7 @@ with open('date_notnull_circle_pack.csv') as csvfile:
 print result
 
 
-# variant = {}
-# variant = {"name": "variants", "children": result}
-# with open('data.json', 'w') as outfile:
-#     json.dump(variant, outfile)
+variant = {}
+variant = {"name": "variants", "children": result}
+with open('data.json', 'w') as outfile:
+    json.dump(variant, outfile)
